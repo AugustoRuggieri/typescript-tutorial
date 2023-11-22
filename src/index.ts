d00bb75bdbfb6d6b20a953bcf6c1ad782ac31b79
augustoruggieri <augustoruggieri@gmail.com>
2023-11-22 15:05:22 +0100

    Adds complex interface example

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,33 @@ let user: User;
 user = {id: 1, name: 'Augusto', surname: 'Ruggieri'};
 
 log(`${user.name} ${user.surname}`);
+
+
+
+// Esempio: tipizzare un oggetto complesso
+
+interface ComplexUser {
+    id: number,
+    name: string,
+    surname: string,
+    /* coords: {
+        lat: number,
+        lng: number
+    } */
+    coords: Coordinates
+}
+
+interface Coordinates {
+    lat: number,
+    lng: number
+}
+
+const complexUser: ComplexUser = {
+    id: 2,
+    name: 'Gemma',
+    surname: 'Arterton',
+    coords: {
+        lat: 123,
+        lng: 456
+    }
+}
